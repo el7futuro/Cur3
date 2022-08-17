e2b62ba87e0725e817aa364de65bd1fa4b574f53
el7futuro <el7futuro@gmail.com>
2022-08-18 00:36:46 +0500

    3.1

---
+++ main.py
@@ -0,0 +1,25 @@
+from flask import Flask
+
+from app.api.views import bluepint_api
+from app.posts.views import posts_blueprint
+
+app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
+
+app.register_blueprint(posts_blueprint)
+app.register_blueprint(bluepint_api)
+
+
+
+@app.errorhandler(404)
+def page_error_404(error):
+    return f"Такой страницы нет {error}", 404
+
+
+@app.errorhandler(500)
+def page_error_500(error):
+    return f"На сервере ошибка - {error}", 500
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
